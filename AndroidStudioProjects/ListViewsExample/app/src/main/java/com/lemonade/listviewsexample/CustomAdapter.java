6b891e9af2b9e8ccab9b4e9ca04ba6132c4ddf07
Paul Kim <paulkim@Pauls-MacBook-Air.local>
2015-12-18 00:36:11 -0500

    first commit

---
+++ AndroidStudioProjects/ListViewsExample/app/src/main/java/com/lemonade/listviewsexample/CustomAdapter.java
@@ -0,0 +1,7 @@
+package com.lemonade.listviewsexample;
+
+/**
+ * Created by paulkim on 11/29/15.
+ */
+public class CustomAdapter {
+}
---
+++ AndroidStudioProjects/Memes/app/src/main/java/com/lemonade/memes/BottomPictureFragment.java
@@ -0,0 +1,7 @@
+package com.lemonade.memes;
+
+/**
+ * Created by paulkim on 11/5/15.
+ */
+public class BottomPictureFragment {
+}
